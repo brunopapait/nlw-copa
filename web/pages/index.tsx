07257cc5867081e40596b4662b75efab0f2ea5fa
brunopapait <brunohpapait@gmail.com>
2022-10-31 21:56:12 -0300

    feat: init fontend app

---
+++ web/pages/index.tsx
@@ -0,0 +1,19 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count").then(
+    (response) => response.json()
+  );
+
+  return {
+    props: {
+      count: response.count,
+    },
+  };
+};
