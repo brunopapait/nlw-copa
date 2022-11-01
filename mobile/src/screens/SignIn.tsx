cf4a87a8a4b7e74313ed746fa3efcc03e95a4007
brunopapait <brunohpapait@gmail.com>
2022-11-01 00:00:13 -0300

    feat: init app mobile

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gay.900">
+      <Text color="white" fontSize={24}>
+        Hello world
+      </Text>
+    </Center>
+  );
+}
