1d0ba5b86518db3fd3e96674016db053800e697c
brunopapait <brunohpapait@gmail.com>
2022-11-06 20:55:38 -0300

    feat: config app to use files svg and create types of files .svg

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
 import { guessRoutes } from "./routes/guess";
+import { authRoutes } from "./routes/auth";
 
 // Singleton -> only one instance of the PrismaClient is created
 
@@ -18,6 +19,7 @@ async function bootstrap() {
   fastify.register(poolRoutes);
   fastify.register(userRoutes);
   fastify.register(guessRoutes);
+  fastify.register(authRoutes);
 
   await fastify.listen({ port: 3333 /*host: '0.0.0.0' */ });
 }
