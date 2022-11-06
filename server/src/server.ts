38ba997a1b79030bc4cae350f4df1f44b19682d0
brunopapait <brunohpapait@gmail.com>
2022-11-06 20:37:39 -0300

    feat: separated routes into files by resource

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
-import { z } from "zod";
-import ShortUniqueId from "short-unique-id";
+import { poolRoutes } from "./routes/pool";
+import { userRoutes } from "./routes/user";
+import { guessRoutes } from "./routes/guess";
 
-const prisma = new PrismaClient({
-  log: ["query", "info", "warn"],
-});
+// Singleton -> only one instance of the PrismaClient is created
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -17,39 +15,9 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count();
-    return { count };
-  });
-
-  fastify.get("/users/count", async () => {
-    const count = await prisma.user.count();
-    return { count };
-  });
-
-  fastify.get("/guesses/count", async () => {
-    const count = await prisma.user.count();
-    return { count };
-  });
-
-  fastify.post("/pools", async (request, response) => {
-    const createPoolBody = z.object({
-      title: z.string(),
-    });
-    const { title } = createPoolBody.parse(request.body);
-
-    const generate = new ShortUniqueId({ length: 6 });
-    const code = String(generate()).toUpperCase();
-
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    return response.status(201).send({ code });
-  });
+  fastify.register(poolRoutes);
+  fastify.register(userRoutes);
+  fastify.register(guessRoutes);
 
   await fastify.listen({ port: 3333 /*host: '0.0.0.0' */ });
 }
