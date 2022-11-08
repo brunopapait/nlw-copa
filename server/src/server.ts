51db94864c08d3aa9cac864645d97349a49163f4
brunopapait <brunohpapait@gmail.com>
2022-11-08 00:02:46 -0300

    feat: finish backend app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
 import { guessRoutes } from "./routes/guess";
 import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
 
 // Singleton -> only one instance of the PrismaClient is created
 
@@ -16,10 +18,16 @@ async function bootstrap() {
     origin: true,
   });
 
+  // Colocar a secret key em um arquivo .env
+  fastify.register(jwt, {
+    secret: "nlwCopa2022",
+  });
+
   fastify.register(poolRoutes);
   fastify.register(userRoutes);
   fastify.register(guessRoutes);
   fastify.register(authRoutes);
+  fastify.register(gameRoutes);
 
   await fastify.listen({ port: 3333 /*host: '0.0.0.0' */ });
 }
